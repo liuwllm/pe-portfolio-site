f69c1be3a5eae77f8741ac4c0c323b42709e6323
liuwllm <liu.wllm@gmail.com>
2025-06-20 17:41:53 -0400

    Add work experiences

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    work_experiences = [
+        {
+            "picture": "streamforge.jpg",
+            "title": "Software Engineer Intern",
+            "company": "Streamforge",
+            "date": "Apr 2023 - Aug 2023",
+            "description": "Developed POC projects for new and existing data pipelines and managed influencer advertising campaigns for Remnant II and Sea of Stars, generating thousands of clickthroughs"
+        },
+        {
+            "picture": "cafezia.jpg",
+            "title": "UX Intern",
+            "company": "Cafezia Coffee",
+            "date": "Nov 2022 - Apr 2023",
+            "description": "Provided improvements to user experience, search engine optimization, and customer engagementProvided improvements to user experience, search engine optimization, and customer engagement"
+        }
+    ]
+
+    return render_template('index.html', title="MLH Fellow", work_experience=work_experiences, url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', title="Hobbies", url=os.getenv("URL"))
