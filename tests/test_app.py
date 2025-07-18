22801b9b3b7d1687bf2cbd2c148b630aa1e01d77
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-07-18 10:28:10 -0700

    developed: extra test cases

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,7 +15,9 @@ class AppTestCase(unittest.TestCase):
         assert response.status_code == 200
         html = response.get_data(as_text=True)
         assert "<title>MLH Fellow</title>" in html
-        # TODO Add more tests relating to the home page
+        assert "About Me" in html
+        assert "Work Experience" in html
+        assert "Education" in html
 
     def test_timeline(self):
         response = self.client.get("/api/timeline_post")
@@ -24,5 +26,39 @@ class AppTestCase(unittest.TestCase):
         json = response.get_json()
         assert "timeline_posts" in json
         assert len(json["timeline_posts"]) == 0
-        # TODO Add more tests relating to the /api/timeline_post GET and POST apis
-        # TODO Add more tests relating to the timeline page
+        # Test POST endpoint for timeline
+        post_response = self.client.post("/api/timeline_post", data=
+            {"name": "Tom Bow", "email": "tom@example.com", "content": "Another test!"})
+        assert post_response.status_code == 200
+        assert response.is_json
+
+        # Test that timeline post was successfully added to database with GET endpoint
+        get_response = self.client.get("/api/timeline_post")
+        assert get_response.is_json
+        get_json = get_response.get_json()
+        entry = get_json["timeline_posts"][0]
+        assert entry["name"] == "Tom Bow"
+        assert entry["email"] == "tom@example.com"
+        assert entry["content"] == "Another test!"
+
+    def test_malformed_timeline_post(self):
+        # POST request missing name
+        response = self.client.post("/api/timeline_post",
+                                    data={"email": "john@example.com", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid name" in html
+
+        # POST request with empty content
+        response = self.client.post("/api/timeline_post", 
+                                    data={"name": "John Doe", "email": "john@example.com", "content": ""})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid content" in html
+
+        # POST request with malformed email
+        response = self.client.post("/api/timeline_post", 
+                                    data={"name": "John Doe", "email": "not-an-email", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid email" in html
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,60 +1,36 @@
-# test_db.py
-
 import unittest
-import os
-os.environ['TESTING'] = 'true'
-
 from peewee import *
-from app import TimelinePost, app
+
+from app import TimelinePost
+
+MODELS = [TimelinePost]
+
+test_db = SqliteDatabase(':memory:')
 
 class TestTimelinePost(unittest.TestCase):
     def setUp(self):
-        self.client = app.test_client()
+        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+        test_db.connect()
+        test_db.create_tables(MODELS)
+
+    def tearDown(self):
+        test_db.drop_tables(MODELS)
+        test_db.close()
 
     def test_timeline_post(self):
-        # Create 2 timeline posts.
-        first_post = TimelinePost.create(name='John Doe',
-                                         email='john@example.com', 
-                                         content='Hello world, I\'m John!')
+        first_post = TimelinePost.create(name='Aung', email='aung@example.com', content='Hello world, I\'m Aung!')
         assert first_post.id == 1
-
-        second_post = TimelinePost.create(name='Jane Doe',
-                                          email='jane@example.com', 
-                                          content='Hello world, I\'m Jane!')
+        second_post = TimelinePost.create(name='William', email='william@example.com', content='Hello world, I\'m William!')
         assert second_post.id == 2
 
-        # Get timeline posts and assert that they are correct
-        response = self.client.get("/api/timeline_post")
-        assert response.status_code == 200
-        json = response.get_json()
-        assert "timeline_posts" in json
-        assert len(json["timeline_posts"]) == 2
-        
-        # Check that posts are in descending order (newest first)
-        assert json["timeline_posts"][0]["name"] == "Jane Doe"
-        assert json["timeline_posts"][1]["name"] == "John Doe"
 
-    def test_malformed_timeline_post(self):
-        # POST request missing name
-        response = self.client.post("/api/timeline_post", data={
-            "email": "john@example.com", "content": "Hello world, I'm John!"
-        })
-        assert response.status_code == 400
-        html = response.get_data(as_text=True)
-        assert "Invalid name" in html
+        retrieved_first_post = TimelinePost.get_by_id(1)
+        assert retrieved_first_post.name == 'Aung'
+        assert retrieved_first_post.email == 'aung@example.com'
 
-        # POST request with empty content
-        response = self.client.post("/api/timeline_post", data={
-            "name": "John Doe", "email": "john@example.com", "content": ""
-        })
-        assert response.status_code == 400
-        html = response.get_data(as_text=True)
-        assert "Invalid content" in html
+        retrieved_second_post = TimelinePost.get_by_id(2)
+        assert retrieved_second_post.name == 'William'
+        assert retrieved_second_post.email == 'william@example.com'
+        assert retrieved_second_post.content == 'Hello world, I\'m William!'
 
-        # POST request with malformed email
-        response = self.client.post("/api/timeline_post", data={
-            "name": "John Doe", "email": "not-an-email", "content": "Hello world, I'm John!"
-        })
-        assert response.status_code == 400
-        html = response.get_data(as_text=True)
-        assert "Invalid email" in html
+     
